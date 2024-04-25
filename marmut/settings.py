e14ccac084670fe19c6a25fd809c87febd3fed97
firedfrogg <julianalex2307@gmail.com>
2024-04-25 21:31:02 +0700

    connecting to the database

--- marmut/settings.py
+++ marmut/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'marmut.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'NAME': 'postgres',
+        'USER': 'postgres.kjbsshuatchylgygwyzo',
+        'PORT': '5432',
+        'PASSWORD': 'marmut-basdat',
     }
 }
 
